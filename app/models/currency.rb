80aed7dd94efb770068e8f00bac8c4327b82fe84
Oktay Donmez <oktaydonmez98@gmail.com>
2022-10-28 23:33:46 +0300

    add currency model

---
+++ app/models/currency.rb
@@ -0,0 +1,12 @@
+# == Schema Information
+#
+# Table name: currencies
+#
+#  id          :bigint           not null, primary key
+#  description :string
+#  name        :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+class Currency < ApplicationRecord
+end
